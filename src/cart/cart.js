74c6f4b96d65477ed0412b080626db37553272f2
shaharb-starkware <shahar.b@starkware.co>
2024-08-24 22:26:07 +0300

    add interview questions

---
+++ src/cart/cart.js
@@ -0,0 +1,44 @@
+// Maps product -> price per unit
+const products = {
+    eggs: 10,
+    jam: 6,
+    pasta: 9,
+    tea: 4,
+    bread: 12
+};
+
+const coupons = [
+    {
+        valid: ['eggs'],
+        discount: 10, // %
+        minQuantity: 2
+    },
+    {
+        valid: ['tea', 'bread'],
+        discount: 15
+    },
+    {
+        valid: ['tea'],
+        discount: 5,
+        minQuantity: 3
+    }
+];
+
+
+// Map product -> quantity
+const cart = {
+    eggs: 2,
+    jam: 1,
+    bread: 1,
+    tea: 4
+};
+
+
+let finalPrice = 7;
+
+// your code goes here
+function calcCartPrice(withCoupons = false) {
+
+}
+
+console.log(`Your cart price is: ${finalPrice}`);
